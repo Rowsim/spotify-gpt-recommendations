3305278a39ee88d7dea8af634f9c50876d6161ea
Rowsim <rowanq@live.co.uk>
2023-07-12 11:55:43 +0100

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p>
-          React app!
-        </p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
