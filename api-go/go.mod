72b3d019cfb0305d72fce41f1b0ce2b8eb840c1a
Rowsim <rowanq@live.co.uk>
2023-09-11 20:31:17 +0100

    add spotify request to go authoriser

--- api-go/go.mod
+++ api-go/go.mod
@@ -3,3 +3,5 @@ module spotify-gpt-receommendations-go
 go 1.21.1
 
 require github.com/aws/aws-lambda-go v1.41.0
+
+require github.com/imroc/req v0.3.2
--- api-go/src/functions/authorizer/main.go
+++ api-go/src/functions/authorizer/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/imroc/req"
 )
 
 func Handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
-	if request.Headers["authorization"] != os.Getenv("TEST_AUTH_KEY") {
+	headerToken := request.Headers["authorization"]
+	hasSpotifyToken := checkSpotifyUser(headerToken)
+	if headerToken != os.Getenv("TEST_AUTH_KEY") || !hasSpotifyToken {
 		return events.APIGatewayV2CustomAuthorizerSimpleResponse{
 			IsAuthorized: false,
 		}, nil
@@ -21,4 +25,20 @@ func Handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2CustomA
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func checkSpotifyUser(authorizationToken string) bool {
+	requestClient := req.New()
+	authHeader := req.Header{
+		"Authorization": fmt.Sprintf("Bearer %s", authorizationToken),
+	}
+
+	_, err := requestClient.Get("https://api.spotify.com/v1/me", authHeader)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		return false
+	}
+
+	return true
 }
